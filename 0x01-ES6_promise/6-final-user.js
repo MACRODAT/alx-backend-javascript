622b1a41730689ae4d9f057515a42b6267f8ad22
MACRODAT <macrodatservices@gmail.com>
2024-08-01 00:15:03 +0100

    Changes to the file

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -5,9 +5,9 @@ export default async function handleProfileSignup(firstName, lastName, fileName)
   return Promise
     .allSettled([signUpUser(firstName, lastName), uploadPhoto(fileName)])
     .then((res) => (
-      res.map((o) => ({
-        status: o.status,
-        value: o.status === 'fulfilled' ? o.value : String(o.reason),
+      res.map((state) => ({
+        status: state.status,
+        value: state.status === 'fulfilled' ? state.value : String(state.reason),
       }))
     ));
 }
