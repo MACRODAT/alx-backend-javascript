12d960eb871bf965e10f771f9ce0ad61c2d31a8b
MACRODAT <macrodatservices@gmail.com>
2024-09-21 16:53:45 +0100

    Changes to the file

--- 0x05-Node_JS_basic/2-read_file.js
+++ 0x05-Node_JS_basic/2-read_file.js
@@ -1,33 +1,31 @@
 const fs = require('fs');
 
 const CountStudents = (db) => {
-	if (!fs.existsSync(db))
-	{
-		throw new Error('Cannot load the database');
-	}
-	const data = fs.readFileSync(db, encoding='utf-8').toString().trim();
-	const lines = data.split('\n');
-	const columns = lines[0].split(',');
-	const groups = {};
+  if (!fs.existsSync(db)) {
+    throw new Error('Cannot load the database');
+  }
+  const data = fs.readFileSync(db, 'utf-8').toString().trim();
+  const lines = data.split('\n');
+  const columns = lines[0].split(',');
+  const groups = {};
   let nbr = 0;
   for (const line of lines.slice(1)) {
     const data = line.split(',');
-    if (data == []) {
-      continue;
+    if (data !== []) {
+      nbr += 1;
+      const field = data[columns.length - 1];
+      if (!Object.keys(groups).includes(field)) {
+        groups[field] = [data[0]];
+      } else {
+        groups[field].push(data[0]);
+      }
     }
-    nbr = nbr + 1;
-    const field = data[columns.length - 1];
-    if (!Object.keys(groups).includes(field)) {
-      groups[field] = [data[0]];
-    } else {
-      groups[field].push(data[0]);
-    }
-	}
-  console.log('Number of students: ' + nbr);
+  }
+  console.log(`Number of students: ${nbr}`);
   for (const [group, students] of Object.entries(groups)) {
-    let s = 'Number of students in ' + String(group) + ': ' + students.length + '. List: ' + (students).join(', ');
+    const s = `Number of students in ${String(group)}: ${students.length}. List: ${students.join(', ')}`;
     console.log(s);
   }
-}
+};
 
 module.exports = CountStudents;
