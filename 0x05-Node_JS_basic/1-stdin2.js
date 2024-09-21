4c1bc7f5fb71887ec2b5e59bc301cb7afba857b0
MACRODAT <macrodatservices@gmail.com>
2024-09-21 16:08:21 +0100

    Changes to the file

--- 0x05-Node_JS_basic/1-stdin2.js
+++ 0x05-Node_JS_basic/1-stdin2.js
@@ -1,10 +1,13 @@
 process.stdout.write('Welcome to Holberton School, what is your name?\n');
+
 process.stdin.on('readable', () => {
-  const z = process.stdin.read();
-  if (z) {
-    process.stdout.write(`Your name is: ${z}`);
+  const chunk = process.stdin.read();
+
+  if (chunk) {
+    process.stdout.write(`Your name is: ${chunk}`);
   }
 });
+
 process.stdin.on('end', () => {
   process.stdout.write('This important software is now closing\n');
 });
